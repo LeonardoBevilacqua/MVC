5d353a44f3c09ff6ee88e30d75f1defb9208c009
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2016-12-23 23:00:07 -0200

    Ajuste no sistema de layouts

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,6 +2,19 @@
 
 class Controller
 {
+    protected $header = '_layout/header';
+    protected $footer = '_layout/footer';
+
+    function setHeader($header) 
+    {
+        $this->header = $header;
+    }
+
+    function setFooter($footer) 
+    {
+        $this->footer = $footer;
+    }
+
     /**
      * Load the object of the model.
      * @param type $model The location of the model file.
@@ -16,20 +29,37 @@ class Controller
      * Load the view.
      * @param type String The location of the view;
      * @param type Array (Optional) The data to use in the view.
+     * @param type Bool (Optional) Set include of layout.
      */
-    public function view($view, $data = [])
+    public function view($view, $data = [], $noInclude = false)
     {
-        if(file_exists('app/views/header.php'))
+        if($noInclude)
         {
-            require_once 'app/views/header.php';
+            require_once 'app/views/' . $view . '.php';
         }
-        
-        require_once 'app/views/' . $view . '.php';
-        
-        if(file_exists('app/views/footer.php'))
+        else
         {
-            require_once 'app/views/footer.php';
+            $this->_renderHeader($this->header, $data);
+
+            require_once 'app/views/' . $view . '.php';
+
+            $this->_renderFooter($this->footer, $data);
         }
     }
+        
+    private function _renderHeader($header, $data = [])
+    {
+        if(file_exists('app/views/' . $header . '.php'))
+            {
+                require_once 'app/views/' . $header . '.php';
+            }
+    }
     
+    private function _renderFooter($footer, $data = [])
+    {
+        if(file_exists('app/views/' . $footer . '.php'))
+            {
+                require_once 'app/views/' . $footer . '.php';
+            }
+    }
 }
---
+++ app/views/_custom/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+    <title>Test</title>
+    <link rel="stylesheet" href="/MVC/public/css/default.css" />    
+    <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/themes/sunny/jquery-ui.css" />
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="/MVC/public/js/custom.js"></script>
+</head>
+<body>
+
+
+    
+<div id="header">
+
+
+        <a href="index">Index</a>
+        <a href="help">Help</a>
+        <a href="login">Login</a>
+
+</div>
+    
+<div id="content">
+    
+    
